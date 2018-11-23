b384b2dc3c0e4f42ce56b1b3ec580cf3fa4fca33
lartl <luis.arce22@gmail.com>
2018-11-23 08:35:10 -0400

    Monthly chart

--- src/app/pages/rep/rep-sells/rep-sells.component.ts
+++ src/app/pages/rep/rep-sells/rep-sells.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
+import { Chart } from "chart.js";
 
 import {
   RepSellsService,
@@ -13,12 +14,57 @@ import {
 })
 export class RepSellsComponent implements OnInit {
   sells: ISell[];
+  @ViewChild("monthlyChart") private chartRef;
+  monthlyChart: any;
   constructor(private sellsSrv: RepSellsService, private router: Router) {
     this.sells = [];
   }
 
   ngOnInit() {
-    this.sellsSrv.getAll().subscribe(p => (this.sells = p));
+    this.sellsSrv.getAll().subscribe(p => {
+      this.sells = p;
+      let monthlyData: Array = new Array();
+      this.sells.forEach(sell => {
+        let createdDate = new Date(sell.createdDate);
+        let keyData = createdDate.getFullYear() + "-" + (createdDate.getMonth() + 1);
+        if (monthlyData[keyData]) {
+          monthlyData[keyData] = monthlyData[keyData] + Number(sell.totalPrice);
+        } else {
+          monthlyData[keyData] = Number(sell.totalPrice);
+        }
+      });
+
+      this.monthlyChart = new Chart(this.chartRef.nativeElement, {
+        type: "bar",
+        data: {
+          labels: Object.keys(monthlyData),
+          datasets: [
+            {
+              data: Object.values(monthlyData),
+              borderColor: "#00AEFF",
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [
+              {
+                display: true
+              }
+            ],
+            yAxes: [
+              {
+                display: true
+              }
+            ]
+          }
+        }
+      });
+    });
   }
 
   openSellRep(sellId: string) {
